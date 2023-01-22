6bcde570ba133e1a87a2a2cd960a6c669e0cbaf8
denisfilonov <dean.mitz6@gmail.com>
2023-01-22 17:01:58 +0100

    Add project files.

---
+++ Exam/CouplePictures.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace Exam
+{
+    public enum Pictures
+    {
+        Aston = 1,
+        Ferrari = 2,
+        Hyundai = 3,
+        Koenigsegg = 4,
+        Lamborghini = 5,
+        Mercedes = 6,
+        Mitsuibishi = 7,
+        Porsche = 8,
+        Subaru = 9,
+        Zenovo = 10
+    }
+
+    internal class CouplePictures
+    {
+        private int[] _testArray = new int[20];
+        private int _conditionCounter = 0;
+        private Button[] _xBox;
+        private int _counter = 0;
+        private int[] _pressed = new int[2];
+        private bool[] _opened = new bool[20];
+
+
+        public CouplePictures(Button[] box, int[] TArray)
+        {
+            _xBox = box;
+            _testArray = TArray;
+
+            for (int i = 0; i < 20; i++)
+            {
+                box[i].Content = "";
+            }
+        }
+
+        public void ClassOnClick(Button[] box, int index)
+        {
+            if (_counter == 2 || _opened[index] || _counter == 1 && _pressed[0] == index) return;
+
+            _xBox[index].Content = SetPicture((Pictures)_testArray[index]);
+            _pressed[_counter] = index;
+            _counter++;
+            if (_counter == 2)
+            {
+                ButtonCompare(box, _pressed[0], _pressed[1]);
+            }
+        }
+
+        public StackPanel SetPicture(Pictures test)
+        {
+            test.ToString();
+            Image img = new Image();
+            StackPanel stackPnl = new StackPanel();
+            img.Source = new BitmapImage(new Uri(test.ToString() + ".jpg", UriKind.Relative));
+            stackPnl.Orientation = Orientation.Horizontal;
+            stackPnl.Margin = new Thickness(-2);
+            stackPnl.Children.Add(img);
+            return stackPnl;
+        }
+
+        public void ButtonCompare(Button[] box, int check1, int check2)
+        {
+            if (_testArray[check1] == _testArray[check2])
+            {
+                _conditionCounter++;
+                _opened[check1] = true;
+                _opened[check2] = true;
+
+                _xBox[check1].Visibility = Visibility.Collapsed;
+                _xBox[check2].Visibility = Visibility.Collapsed;
+
+                if (_conditionCounter == 10)
+                {
+                    MessageBox.Show("You Won!!!", "Congratulations", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                }
+                _counter = 0;
+            }
+            else
+            {
+                var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(0.5) };
+                timer.Start();
+                timer.Tick += (sender, args) =>
+                {
+                    timer.Stop();
+                    _xBox[check1].Content = "";
+                    _xBox[check2].Content = "";
+                    _counter = 0;
+                };
+            }
+        }
+
+        public void Reset(Button[] box)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                _xBox[i].Content = "";
+                _xBox[i].Visibility = Visibility.Visible;
+            }
+        }
+
+        public void Available(Button[] box)
+        {
+            foreach (var item in box)
+            {
+                item.IsEnabled = true;
+            }
+        }
+
+        public void NotAvailable(Button[] box)
+        {
+            foreach (var item in box)
+            {
+                item.IsEnabled = false;
+            }
+        }
+    }
+}
---
+++ Exam/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Exam
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CouplePictures _currentGame;
+        private Button[] _buttons;
+        private Random _rndGenerate;
+        private int[] _profInputList;
+        private int _amountOfTry { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _buttons = new Button[20];
+            _rndGenerate = new Random();
+            _profInputList = new int[20];
+            lblAmountOfTry.Content = _amountOfTry;
+            lblAmountOfTry.Visibility = Visibility.Hidden;
+        }
+        private void startGame_Click(object sender, RoutedEventArgs e)
+        {
+            if (_currentGame == null)
+            {
+                var test = Enumerable.Range(1, 20).OrderBy(x => _rndGenerate.Next()).ToArray();
+                for (int i = 0; i < 20; i++)
+                {
+                    _buttons[i] = gridPictures.Children[i] as Button;
+                    _profInputList[i] = (test[i] - 1) % 10 + 1;
+                }
+                _currentGame = new CouplePictures(_buttons, _profInputList);
+                startGame.IsEnabled = false;
+                MessageBox.Show("The game started! Good luck", "Notification", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                lblAmountOfTry.Visibility = Visibility.Visible;
+                checkBoxEasy.Visibility = Visibility.Hidden;
+                checkBoxEasy_Checked(sender, e);
+            }
+        }
+
+        private void resetGame_Click(object sender, RoutedEventArgs e)
+        {
+            if (_currentGame != null)
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    _currentGame.Reset(_buttons);
+                }
+                _currentGame.Available(_buttons);
+                MessageBox.Show("The game restarted! Good luck", "Notification", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                _currentGame = null;
+                startGame.IsEnabled = true;
+                _amountOfTry = 30;
+                lblAmountOfTry.Content = _amountOfTry;
+                lblAmountOfTry.Foreground = Brushes.Green;
+                lblAmountOfTry.Visibility = Visibility.Visible;
+                checkBoxEasy.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                MessageBox.Show("First start the game", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void mtpButtonsOnclick(object sender, RoutedEventArgs e)
+        {
+            if (_currentGame == null)
+            {
+                MessageBox.Show("First press Start button to start the game", "Notification", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+            else
+            {
+                int buttonName = int.Parse((sender as Button).Name.Substring(6)) - 1;
+                _currentGame.ClassOnClick(_buttons, buttonName);
+                CheckTheAmount();
+            }
+        }
+
+        private void CheckTheAmount()
+        {
+            lblAmountOfTry.Content = --_amountOfTry;
+            if (_amountOfTry == 20)
+            {
+                lblAmountOfTry.Foreground = Brushes.Orange;
+            }
+            else if (_amountOfTry == 10)
+            {
+                lblAmountOfTry.Foreground = Brushes.Yellow;
+            }
+            else if (_amountOfTry == 5)
+            {
+                lblAmountOfTry.Foreground = Brushes.Red;
+            }
+            else if (_amountOfTry == 0)
+            {
+                lblAmountOfTry.Foreground = Brushes.Black;
+                lblAmountOfTry.Content = "End";
+
+                for (int i = 0; i < 20; i++)
+                {
+                    _currentGame.Reset(_buttons);
+                }
+                _currentGame.NotAvailable(_buttons);
+                lblAmountOfTry.Visibility = Visibility.Hidden;
+                MessageBox.Show("You have exhausted the number of tries, the game is over", "Defeat", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void checkBoxEasy_Checked(object sender, RoutedEventArgs e)
+        {
+            if (checkBoxEasy.IsChecked == true)
+            {
+                _amountOfTry = 50;                
+                lblAmountOfTry.Content = _amountOfTry;
+            }
+            else
+            {
+                _amountOfTry = 30;
+                lblAmountOfTry.Content = _amountOfTry;
+            }
+        }
+    }
+}
